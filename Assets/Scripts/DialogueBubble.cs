bdcc0076755acf2acdc43c92b0672aee01e3b177
princekin116 <princekin.nicholas@gmail.com>
2023-02-05 04:29:06 +0800

    update 转场

--- Assets/Scripts/DialogueBubble.cs
+++ Assets/Scripts/DialogueBubble.cs
@@ -6,12 +6,18 @@ public class DialogueBubble : MonoBehaviour
 {
     private bool alreadyTriggerDialogue;
     [SerializeField] private GameObject speaker;
+    private DialogueCommand bossSpeaking = new DialogueCommand("Hello World", "Boss", Vector2.zero);
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == Service.playerTag && !alreadyTriggerDialogue)
         {
             alreadyTriggerDialogue = true;
-            EventHandler.Call_UI_OnShowDialogueBubble(new DialogueCommand("Hello World", "Boss", Vector2.zero));
+            EventHandler.Call_UI_OnShowDialogueBubble(bossSpeaking);
         }
+        else if(alreadyTriggerDialogue)
+        {
+            //Temp solution
+            EventHandler.Call_UI_OnHideDialougeBubble(bossSpeaking);
+        }        
     }
 }
