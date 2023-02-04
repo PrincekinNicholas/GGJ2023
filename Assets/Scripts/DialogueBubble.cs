1594bf5e32f3ed3fc29a1fbbeeb65fcea115850d
paledust <paledust@hotmail.com>
2023-02-05 05:43:14 +0800

    更新dialogueBubble的功能

--- Assets/Scripts/DialogueBubble.cs
+++ Assets/Scripts/DialogueBubble.cs
@@ -5,19 +5,14 @@ using UnityEngine;
 public class DialogueBubble : MonoBehaviour
 {
     private bool alreadyTriggerDialogue;
+    [SerializeField] private DialogueCommand bossDialogue;
     [SerializeField] private GameObject speaker;
-    private DialogueCommand bossSpeaking = new DialogueCommand("Hello World", "Boss", Vector2.zero);
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == Service.playerTag && !alreadyTriggerDialogue)
         {
             alreadyTriggerDialogue = true;
-            EventHandler.Call_UI_OnShowDialogueBubble(bossSpeaking);
+            EventHandler.Call_UI_OnShowDialogueBubble(bossDialogue);
         }
-        else if(alreadyTriggerDialogue)
-        {
-            //Temp solution
-            EventHandler.Call_UI_OnHideDialougeBubble(bossSpeaking);
-        }        
     }
 }
