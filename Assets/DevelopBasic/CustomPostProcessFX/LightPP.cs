c337d06177295f09d20d9779173debbb3ca79ef8
paledust <paledust@hotmail.com>
2023-02-05 14:56:02 +0800

    修复post process 问题

--- Assets/DevelopBasic/CustomPostProcessFX/LightPP.cs
+++ Assets/DevelopBasic/CustomPostProcessFX/LightPP.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 
 [Serializable]
-[PostProcess(typeof(LightPPRenderer), PostProcessEvent.AfterStack, "Custom/LightPP")]
+[PostProcess(typeof(LightPPRenderer), PostProcessEvent.BeforeStack, "Custom/LightPP")]
 public class LightPP : PostProcessEffectSettings
 {
-    [Range(0f, 1f), Tooltip("Grayscale effect intensity.")]
+    [Range(0f, 100f), Tooltip("Grayscale effect intensity.")]
     public FloatParameter intensity = new FloatParameter { value = 0f };
     [Range(0f, 1f)]
     public FloatParameter darkness = new FloatParameter { value = 0f };
@@ -19,7 +19,7 @@ public sealed class LightPPRenderer : PostProcessEffectRenderer<LightPP>
     public override void Render(PostProcessRenderContext context)
     {
         var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/LightPP"));
-        sheet.properties.SetFloat("_Itensity", settings.intensity);
+        sheet.properties.SetFloat("_Intensity", settings.intensity);
         sheet.properties.SetFloat("_Darkness", settings.darkness);
 
         context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
